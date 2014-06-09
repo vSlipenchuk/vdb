6ca9d241584eac9f7f22ceaaeeadd1b70ef5760c
Vladimir Slipenchuk <vSlipenchuk@gmail.com>
2014-06-09 17:20:17 +0400

    vdb2 utils add

---
+++ vdb2.c
@@ -0,0 +1,24 @@
+#include "vdb.h"
+
+int db_config_db(database *db) { // configure database by by a tables (nextN !!!)
+if (db_select(db,"select TYP_SEQ from db where n = 0") && db_fetch(db)) { // Configure a db from db_info
+    db_col *c = db->out.cols;
+    db->typ_seq = db_int(c); // My Next N type
+    }
+return 0;
+}
+
+
+int db_nextN(database *db,char *table) {
+if (db->typ_seq==2) db_selectf(db,"select sq_%s.NextVal from dual",table);
+ else db_selectf(db,"select max(N) from %s",table);
+if (!db_fetch(db)) return 1; // NewOne
+return db_int(db->out.cols)+1;
+}
+
+int db_compilef(database *db,uchar *fmt,...) {
+uchar buf[1024];
+BUF_FMT(buf,fmt);
+//printf("compiling: %s\n",buf);
+return db_compile(db,buf);
+}
